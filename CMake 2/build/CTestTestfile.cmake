f2af533617daaca96817c400ab759f96bb6b83cc
Chen GF <1755371281@qq.com>
2024-10-07 15:32:03 +0800

    fifth

--- CMake 2/build/CTestTestfile.cmake
+++ CMake 2/build/CTestTestfile.cmake
@@ -4,7 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(ctest "/home/chen/CMake Task/CMake 2/build/my_test")
-set_tests_properties(ctest PROPERTIES  _BACKTRACE_TRIPLES "/home/chen/CMake Task/CMake 2/CMakeLists.txt;46;add_test;/home/chen/CMake Task/CMake 2/CMakeLists.txt;0;")
 subdirs("modules")
 subdirs("common")
